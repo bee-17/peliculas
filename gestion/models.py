949d874254f65513ca05afb3c9e9766afca009a0
b127 <xiomara_mercedes_123@hotmail.com>
2026-04-09 00:41:48 -0500

    agregar modelo Director y Película con relación de ForeignKey

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Director(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Pelicula(models.Model):
+    GENEROS = [
+        ('Accion', 'Acción'),
+        ('Comedia', 'Comedia'),
+        ('Drama', 'Drama'),
+        ('Terror', 'Terror'),
+        ('Ciencia Ficcion', 'Ciencia Ficción'),
+        ('Animacion', 'Animación'),
+    ]
+    titulo = models.CharField(max_length=200)
+    anio_estreno = models.IntegerField()
+    genero = models.CharField(max_length=50, choices=GENEROS)
+    director = models.ForeignKey(
+        Director,
+        on_delete=models.CASCADE,
+        related_name='peliculas'
+    )
+
+    def __str__(self):
+        return self.titulo
